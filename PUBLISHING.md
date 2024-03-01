d3c1f5e95b2d10aa73567a4c993c370ab2328f1f
Andrew Seier <andseier@gmail.com>
2024-02-29 16:17:05 -0800

    Modernize repository.
    Updates a handful of things like GitHub Actions configuration, eslint,
    serving, and prepares for publishing to JSR.

--- PUBLISHING.md
+++ PUBLISHING.md
@@ -1,12 +1,38 @@
 # Publishing
 
-We use GitHub actions to publish new versions of this repository. To publish, perform the following actions:
+We use GitHub Actions to publish new versions of this repository from tags. In
+general, this includes some prepublish steps like updating versions in various
+`*.json` files, committing those changes, and tagging the resulting commit.
+These steps can be done manually, or via the `bump` script. Follow whichever
+procedure feels more comfortable. By publishing, the “Publish” GitHub Action
+workflow will be triggered and if all the tests pass, it’ll publish to all the
+registries we care about.
 
-1. Checkout the `main` branch and `pull` to ensure your local branch is current — `git checkout main && git pull origin main`
-2. Run `yarn version` and select the next version number (being careful to differentiate between releases and release candidates) — `yarn version`
-3. Push the resulting commit to the origin's `main` branch — `git push origin main`
-4. Push the resulting tags to origin — `git push origin --tags`
-5. In the GitHub UI, find [the tag you just pushed](https://github.com/Netflix/x-test/tags) and find the "Create release" option.
-6. Add any additional release information (including to check the box if it's a "pre-release", it probably is!).
+Before you start — make sure your on the `main` branch and up to date. E.g., by
+running `git checkout main && git pull origin main`.
 
-By _creating a release_, the "Release" GitHub action will be triggered and if all the tests pass, it'll publish.
+## Publishing with Manual Prepublish
+
+1. Edit `package.json`, `package-lock.json`, and `deno.json` files to
+   set the new version. Don’t forget that `package-lock.json` should be edited
+   in multiple places to account for the self-package’s version (i.e., `""`).
+   For example, set the `"version"` key to a value of `"1.0.0-rc.57"`.
+2. Add / commit those files. By convention, set the message to the new version
+   (e.g., `git commit --message="1.0.0-rc.57"`).
+3. Tag the commit using an annotated tag. By convention, set the name to the new
+   version _prefixed with a “v”_ and set the message to the new version
+   (e.g., `git tag --annotate "v1.0.0-rc.57" --message="1.0.0-rc.57"`).
+4. Push the new commit / tags by running `git push origin main --follow-tags`.
+5. Find [the tag you just pushed](https://github.com/Netflix/x-test/tags) in
+   the GitHub UI and click the “Create release” option. Add any additional
+   release information (including to check the box if it’s a “pre-release”).
+
+## Publishing with Assisted Prepublish (`bump`)
+
+1. Run `npm run bump` to view the current version. Then, run the same command
+   and provide the version to bump to (e.g., `npm run bump 1.0.0-rc.57`). Note,
+   keywords like `major`, `minor`, `patch`, etc. _are_ supported.
+2. Push the new commit / tags by running `git push origin main --follow-tags`.
+3. Find [the tag you just pushed](https://github.com/Netflix/x-test/tags) in
+   the GitHub UI and click the “Create release” option. Add any additional
+   release information (including to check the box if it’s a “pre-release”).
---
+++ server.js
@@ -0,0 +1,102 @@
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+
+const root = process.env.ROOT || '.';
+const userHeaders = JSON.parse(process.env.HEAD || null);
+
+const mimeLookup = new Map([
+  ['.css',  'text/css'],
+  ['.html', 'text/html'],
+  ['.ico',  'image/x-icon'],
+  ['.js',   'application/javascript'],
+  ['.json', 'application/json'],
+  ['.md',   'text/markdown'],
+  ['.png',  'image/png'],
+  ['.txt',  'text/plain'],
+]);
+
+class Server {
+
+  static origin;
+
+  static requestListener(request, response) {
+    if (request.method === 'GET') {
+      const url = new URL(request.url, Server.origin);
+      const filePath = path.resolve(`${root}/${url.pathname}`);
+      const fileExt = path.extname(filePath);
+      const mimeType = mimeLookup.get(fileExt);
+
+      if (fileExt) {
+        if (mimeType) {
+          fs.stat(filePath, (error, stats) => {
+            if (stats) {
+              Server.sendFile(response, filePath, mimeType, stats.size);
+            } else {
+              Server.sendFileNotFound(response);
+            }
+          });
+        } else {
+          Server.sendUnknownMimeType(response, fileExt);
+        }
+      } else {
+        if (url.pathname.endsWith('/')) {
+          const directoryIndex = `${filePath}/index.html`;
+          fs.stat(directoryIndex, (error, stats) => {
+            if (stats) {
+              Server.sendFile(response, directoryIndex, 'text/html', stats.size);
+            } else {
+              // @TODO pushState optional
+              Server.sendRootIndex(response);
+            }
+          });
+        } else {
+          Server.sendRedirect(response, `${url.pathname}/`);
+        }
+      }
+    }
+  }
+
+  static sendUnknownMimeType(response, fileExt) {
+    const message = `Error 500: Unknown MIME type for file extension: ${fileExt}`;
+    response.writeHead(500, { 'Content-Type': 'text/plain', 'Content-Length': message.length, ...userHeaders });
+    response.write(message);
+    response.end();
+  }
+
+  static sendFileNotFound(response) {
+    const message = 'Error 404: Resource not found.';
+    response.writeHead(404, { 'Content-Type': 'text/plain', 'Content-Length': message.length, ...userHeaders });
+    response.write(message);
+    response.end();
+  }
+
+  static sendRedirect(response, location) {
+    response.writeHead(301, { 'Content-Type': 'text/plain', 'Content-Length': 0, location, ...userHeaders });
+    response.end();
+  }
+
+  static sendFile(response, filePath, mimeType, contentLength) {
+    response.writeHead(200, { 'Content-Type': mimeType, 'Content-Length': contentLength, ...userHeaders });
+    fs.createReadStream(filePath).pipe(response);
+  }
+
+  static sendRootIndex(response) {
+    const rootIndex = `${root}/index.html`;
+    fs.stat(rootIndex, (error, stats) => {
+      if (stats) {
+        Server.sendFile(response, rootIndex, 'text/html', stats.size);
+      } else {
+        Server.sendFileNotFound(response);
+      }
+    });
+  }
+}
+
+const server = http.createServer(Server.requestListener);
+
+server.listen(process.env.PORT || 8080, () => {
+  const { address, port } = server.address();
+  Server.origin = `http://[${address}]:${port}`;
+  console.log(`Development server running: ${Server.origin}`); // eslint-disable-line no-console
+});
