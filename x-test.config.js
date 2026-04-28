5517e940b7ebdce3f196d78ee597b16ccc87d3be
Andrew Seier <andseier@gmail.com>
2026-04-28 12:09:43 -0700

    Use pragmas to get “XFrame” coverage to 100%.
    There are a couple places where we strictly _cannot_ test functionality.
    It is desirable to set _exactly_ 100% as a coverage goal so that we
    cannot back-slide.
    
    By excluding the code we cannot test, we can finally set our baseline.

--- x-test.config.js
+++ x-test.config.js
@@ -6,7 +6,7 @@ export default {
   coverageGoals: {
     './x-test.js':          { lines: 100 },
     './x-test-tap.js':      { lines: 100 },
-    './x-test-frame.js':    { lines: 97 },
+    './x-test-frame.js':    { lines: 100 },
     './x-test-root.js':     { lines: 77 },
     './x-test-reporter.js': { lines: 71 },
   },
